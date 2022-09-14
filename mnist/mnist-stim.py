896b0ca16098b4cba77b65d058a50bcbacbb09bd
Katharina <k.duecker@bham.ac.uk>
2022-09-14 16:14:56 +0100

    split into test and train

--- mnist/mnist-stim.py
+++ mnist/mnist-stim.py
@@ -8,41 +8,39 @@ import torch.nn.functional as F
 import math
 
 
-def make_stim():
+def make_stim(train_data=True):
     # download MNIST
-    mnist_trainset = datasets.MNIST(root='./data', train=True, transform=torchvision.transforms.ToTensor(),target_transform=F.one_hot,download=True)
+    mnist = datasets.MNIST(root='./data', train=train_data, transform=torchvision.transforms.ToTensor(),target_transform=F.one_hot,download=True)
 
     # %% [code] {"execution":{"iopub.status.busy":"2022-09-14T15:00:28.827244Z","iopub.execute_input":"2022-09-14T15:00:28.827630Z","iopub.status.idle":"2022-09-14T15:00:29.137653Z","shell.execute_reply.started":"2022-09-14T15:00:28.827599Z","shell.execute_reply":"2022-09-14T15:00:29.136417Z"},"jupyter":{"outputs_hidden":false}}
     # one-hot encode
-    y_train = mnist_trainset.target_transform(mnist_trainset.targets)
+    y = mnist.target_transform(mnist.targets)
 
     # position images into quadrants
-    size_quadr_img = tuple(mnist_trainset.data.shape*np.array((1,2,2)))
+    size_quadr_img = tuple(mnist.data.shape*np.array((1,2,2)))
 
-    sq = mnist_trainset.data.shape[1]
-
-    x_train = torch.zeros(size_quadr_img)
-
-    sq = mnist_trainset.data.shape[1]
+    sq = mnist.data.shape[1]
 
     # first quadrant
-    x_train1 = torch.zeros(size_quadr_img)
-    x_train1[:,0:sq,0:sq] += mnist_trainset.data
+    x1 = torch.zeros(size_quadr_img)
+    x1[:,0:sq,0:sq] += mnist.data
 
     # second quadrant
-    x_train2 = torch.zeros(size_quadr_img)
-    x_train2[:,0:sq,sq:] += mnist_trainset.data        
+    x2 = torch.zeros(size_quadr_img)
+    x2[:,0:sq,sq:] += mnist.data        
 
     # third quadrant
-    x_train3 = torch.zeros(size_quadr_img)
-    x_train3[:,sq:,0:sq] += mnist_trainset.data   
+    x3 = torch.zeros(size_quadr_img)
+    x3[:,sq:,0:sq] += mnist.data   
 
     # fourth quadrant
-    x_train4 = torch.zeros(size_quadr_img)
-    x_train4[:,sq:,sq:] += mnist_trainset.data
+    x4 = torch.zeros(size_quadr_img)
+    x4[:,sq:,sq:] += mnist.data
 
     # concatenate quadrants
-    x_train = torch.concat((x_train1,x_train2,x_train3,x_train4))
+    x = torch.concat((x1,x2,x3,x4))
 
     # repeat targets
-    y_train = torch.tile(y_train,(4,1))
+    y = torch.tile(y,(4,1))
+    
+    return x, y
